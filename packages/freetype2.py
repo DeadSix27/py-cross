380538b1d8af4a52156c55fcb19b838fb4f6dd1a
DeadSix27 <DeadSix27@users.noreply.github.com>
2023-05-13 00:23:36 +0200

    freetype: formatting, stick to stable version

--- packages/freetype2.py
+++ packages/freetype2.py
@@ -12,10 +12,17 @@ class FREETYPE2(BasePackage):
         self.build_system = BasePackage.BuildSystem.Ninja
         self.install_system = BasePackage.BuildSystem.Ninja
         self.source_subfolder = "_build"
+        self.git_tag = "VER-2-13-0"
 
     @property
     def pkg_depends(self):
-        return ( "libpng", 'bzip2', "zlib", "harfbuzz", "brotli" )
+        return (
+            "libpng",
+            'bzip2',
+            "zlib",
+            "harfbuzz",
+            "brotli"
+            )
     
     @property
     def pkg_url(self):
@@ -25,13 +32,15 @@ class FREETYPE2(BasePackage):
     def pkg_config(self):
         return (
             '..',
-            '{cmake_prefix_options}',
-            '-DCMAKE_INSTALL_PREFIX={target_prefix}',
+            "-DCMAKE_TOOLCHAIN_FILE={cmake_toolchain_file}",
+            "-GNinja",
+            "-DCMAKE_BUILD_TYPE=Release",
+            "-DCMAKE_INSTALL_PREFIX={target_prefix}",
             '-DFT_REQUIRE_ZLIB=ON',
             '-DFT_REQUIRE_PNG=ON',
             '-DFT_REQUIRE_HARFBUZZ=ON',
             '-DFT_REQUIRE_REQUIRE_BROTLI=ON',
-            # "-DFT_REQUIRE_BZIP2=ON",
+            "-DFT_REQUIRE_BZIP2=ON",
             )
 
     @property
